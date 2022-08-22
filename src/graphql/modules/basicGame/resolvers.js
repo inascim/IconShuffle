ecfde82e0164313136389694be0e1138f96079a7
Igor Nascimento <igor.f.nascimento@accenture.com>
2022-08-21 23:47:12 -0300

    dev: including pubsub architecture and developing first pubsub endpoint (games list) therefore the next steps should be joining pubsub by the frontend and create the game instances

--- src/graphql/modules/basicGame/resolvers.js
+++ src/graphql/modules/basicGame/resolvers.js
@@ -1,5 +1,24 @@
+import Redis from 'ioredis';
+import { withFilter } from 'graphql-subscriptions';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
 import BasicGame from './model/BasicGame';
 import BasicGameService from './service/basicGameService';
+
+const options = {
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: process.env.REDIS_PORT || '6379',
+  retryStrategy: times => {
+      // reconnect after
+      return Math.min(times * 50, 2000);
+  }
+};
+
+const pubsub = new RedisPubSub({
+  publisher: new Redis(options),
+  subscriber: new Redis(options)
+});
+
+const SOMETHING_CHANGED_TOPIC = 'something_changed';
 
 export default {
   Query: {
@@ -10,5 +29,30 @@ export default {
     createBasicGame: async (_, { data }) => BasicGameService.createBasicGame(data),
     updateBasicGame: async (ctx, { data }) => BasicGameService.updateBasicGame(ctx, data),
     deleteBasicGame: async (_, { id }) => !!(await BasicGame.findOneAndDelete(id)),
+  },
+  Subscription: {
+      hello: {
+      // Example using an async generator
+      subscribe: async function* () {
+        for await (const word of ["Hello", "Bonjour", "Ciao"]) {
+          yield { hello: word };
+        }
+      },
+    },
+    postCreated: {
+      // More on pubsub below
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(['POST_CREATED']),
+        (payload, variables) => console.log({payload, variables})
+        ),
+    },
+    chatCreated: {
+      subscribe: (_parent, _args) => pubsub.asyncIterator(['CHAT_CREATED']),
+    },
+    gamesSub: {
+      subscribe: withFilter(
+        (_, args) => pubsub.asyncIterator(`${SOMETHING_CHANGED_TOPIC}`),
+      ),
+    },
   },
 };
--- src/startServer.js
+++ src/startServer.js
@@ -1,6 +1,18 @@
-import { ApolloServer } from 'apollo-server';
 import mongoose from 'mongoose';
 import { MONGO_URI } from './config';
+import { ApolloServer } from 'apollo-server-express';
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageLocalDefault,
+} from 'apollo-server-core';
+import express from 'express';
+import http from 'http';
+
+
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
+
 
 async function connectDatabase() {
   return new Promise((resolve, reject) => {
@@ -10,11 +22,74 @@ async function connectDatabase() {
       .on('close', () => console.log('Database connection closed.'))
       .once('open', () => resolve(mongoose.connections[0]));
 
-    mongoose.connect(MONGO_URI);
+    mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
   });
 }
 
 async function startServer({ typeDefs, resolvers }) {
+  // Required logic for integrating with Express
+  const app = express();
+  // Our httpServer handles incoming requests to our Express app.
+  // Below, we tell Apollo Server to "drain" this httpServer,
+  // enabling our servers to shut down gracefully.
+  const httpServer = http.createServer(app);
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+  // Creating the WebSocket server
+  const wsServer = new WebSocketServer({
+    // This is the `httpServer` we created in a previous step.
+    server: httpServer,
+    // Pass a different path here if your ApolloServer serves at
+    // a different path.
+    path: '/graphql',
+  });
+  // Hand in the schema we just created and have the
+  // WebSocketServer start listening.
+  const serverCleanup = useServer({ schema }, wsServer);
+  
+  const server = new ApolloServer({ 
+    schema,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+
+  // More required logic for integrating with Express
+  await server.start();
+  server.applyMiddleware({
+    app,
+
+    // By default, apollo-server hosts its GraphQL endpoint at the
+    // server root. However, *other* Apollo Server packages host it at
+    // /graphql. Optionally provide this to match apollo-server.
+    path: '/',
+  });
+
+  const graphqlPort = 3000;
+  await new Promise(
+    resolve => httpServer.listen({ port: process.env.PORT || graphqlPort }, resolve)
+  );
+
+  console.log(
+    `🚀 Query endpoint ready at http://localhost:${graphqlPort}${server.graphqlPath}`
+  );
+
+  console.log(
+    `🚀 Subscription endpoint ready at ws://localhost:${graphqlPort}${server.graphqlPath}`
+  );
+
   try {
     const info = await connectDatabase();
     console.log(`Connected to mongodb 🍃 at ${info.host}:${info.port}/${info.name}`);
@@ -23,13 +98,12 @@ async function startServer({ typeDefs, resolvers }) {
     process.exit(1);
   }
 
-  const server = new ApolloServer({ typeDefs, resolvers });
-  const graphqlPort = 3000;
-  server.setGraphQLPath('graphql');
-  server.listen(process.env.PORT || graphqlPort).then(({ url }) => {
-    console.log(`🚀 Apollo server ready on ${url}`);
-    console.log('⚡️ Playground exposed on /graphql');
-  });
+  // Old Apollo Server Startup
+  // server.setGraphQLPath('graphql');
+  // server.listen(process.env.PORT || graphqlPort).then(({ url }) => {
+  //   console.log(`🚀 Apollo server ready on ${url}`);
+  //   console.log('⚡️ Playground exposed on /graphql');
+  // });
 }
 
 export default startServer;
